2bc10bae7d3c92301a8afbfc839113cb31c952a6
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-02 17:31:52 +0800

    西元2024年12月02日 (週一) 17時31分52秒 CST

--- text-embeddings-inference/docker.sh
+++ text-embeddings-inference/docker.sh
@@ -2,7 +2,7 @@
 
 cd "$(dirname "$0")"
 
-model=jinaai/jina-reranker-v2-base-multilingual
+model=Alibaba-NLP/gte-multilingual-reranker-base
 volume=$PWD/data # share a volume with the Docker container to avoid downloading weights every run
 
 docker run -p 8080:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:cpu-1.5 --model-id $model
