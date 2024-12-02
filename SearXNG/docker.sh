4d1af328b5cd5484c06d2d1f4a081828b4ecf34e
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-02 20:21:27 +0800

    西元2024年12月02日 (週一) 20時21分27秒 CST

--- SearXNG/docker.sh
+++ SearXNG/docker.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 
-docker run --rm -p 8081:8080 -v "./searxng:/etc/searxng" searxng/searxng:2024.12.1-0245e82bd
+docker run --rm -p 40001:8080 -v "./searxng:/etc/searxng" searxng/searxng:2024.12.1-0245e82bd
