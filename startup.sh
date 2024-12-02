cd938d79da147ffb009236faa122269ed5ca43f2
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-02 11:00:10 +0800

    西元2024年12月02日 (週一) 11時00分10秒 CST

--- startup.sh
+++ startup.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-chmod -R 777 /output/*
+cd "$(dirname "$0")"
 
-ls -lh /cache/*.sqlite || true
-# ls -lh / || true
+git pull
+git reset --hard HEAD
 
-export NODE_OPTIONS=--max-old-space-size=4096
-# tor --version
-# service tor start
-#curl --socks5 localhost:9050 --socks5-hostname localhost:9050 -s https://check.torproject.org/ | cat | grep -m 1 Congratulations | xargs
-
-node /app/index.js
-
-# single-file https://blog.pulipuli.info /output/wikipedia.html
+docker-compose up --build
