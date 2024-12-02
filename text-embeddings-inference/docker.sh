419acf9d2ae5624bfb604bf2df21d7bc76e75b44
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-02 17:26:55 +0800

    西元2024年12月02日 (週一) 17時26分55秒 CST

--- text-embeddings-inference/docker.sh
+++ text-embeddings-inference/docker.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 model=Alibaba-NLP/gte-multilingual-reranker-base
 volume=$PWD/data # share a volume with the Docker container to avoid downloading weights every run
 
-docker run --gpus all -p 8080:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:1.5 --model-id $model
+docker run -p 8080:80 -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:1.5 --model-id $model
