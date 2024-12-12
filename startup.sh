6b8ec58784df2235328bf37ef467c45ec239381d
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-13 01:51:50 +0800

    西元2024年12月13日 (週五) 01時51分50秒 CST

--- startup.sh
+++ startup.sh
@@ -4,69 +4,69 @@ full_path=$(realpath "$0")
 base_dir=$(dirname "$full_path")
 cd "$base_dir"
 
-git clean -f -d
-git fetch origin
-git reset --hard origin/main
+# git clean -f -d
+# git fetch origin
+# git reset --hard origin/main
 
-cd ./Dify/docker
+# cd ./Dify/docker
 
-docker-compose down
-docker-compose up --build -d > /dev/null 2>&1 &
+# docker-compose down
+# docker-compose up --build -d > /dev/null 2>&1 &
 
-# ===========
+# # ===========
 
-cd "$base_dir"
-# pwd
-cd ./Firecrawl/
+# cd "$base_dir"
+# # pwd
+# cd ./Firecrawl/
 
-docker-compose down
-docker-compose up -d > /dev/null 2>&1
+# docker-compose down
+# docker-compose up -d > /dev/null 2>&1
 
-# ===========
+# # ===========
 
-cd "$base_dir"
-# pwd
-cd ./SearXNG/
+# cd "$base_dir"
+# # pwd
+# cd ./SearXNG/
 
-# bash ./docker.sh > /dev/null 2>&1 &
-docker-compose down
-docker-compose up -d > /dev/null 2>&1
+# # bash ./docker.sh > /dev/null 2>&1 &
+# docker-compose down
+# docker-compose up -d > /dev/null 2>&1
 
-# ===========
+# # ===========
 
-sleep 5
+# sleep 5
 
-echo $base_dir
-cd "$base_dir"
-# pwd
-cd ./Ollama/
+# echo $base_dir
+# cd "$base_dir"
+# # pwd
+# cd ./Ollama/
 
-docker-compose down
-docker-compose up -d > /dev/null 2>&1
+# docker-compose down
+# docker-compose up -d > /dev/null 2>&1
 
-# sleep 30
+# # sleep 30
 
-# ===========
+# # ===========
 
-sleep 5
+# sleep 5
 
-# https://ithelp.ithome.com.tw/m/articles/10369119
+# # https://ithelp.ithome.com.tw/m/articles/10369119
 
-apt-get update
-apt-get install -y python3-pip
-pip3 install huggingface-hub nltk
+# apt-get update
+# apt-get install -y python3-pip
+# pip3 install huggingface-hub nltk
 
-echo $base_dir
-cd "$base_dir"
-# pwd
-cd ./RAGFlow/
+# echo $base_dir
+# cd "$base_dir"
+# # pwd
+# cd ./RAGFlow/
 
-python3 ./download_deps.py
-docker build -f Dockerfile -t infiniflow/ragflow:dev 
+# python3 ./download_deps.py
+# docker build -f Dockerfile -t infiniflow/ragflow:dev 
 
-cd ./docker/
-docker compose down
-docker compose up -d > /dev/null 2>&1
+# cd ./docker/
+# docker compose down
+# docker compose up -d > /dev/null 2>&1
 
 # docker compose down
 # docker compose up -d > /dev/null 2>&1
