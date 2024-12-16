01000bb09f8eba69a406577e662b417469708945
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2024-12-17 02:25:20 +0800

    西元2024年12月17日 (週二) 02時25分20秒 CST

--- KnowledgeBaseTable/index.js
+++ KnowledgeBaseTable/index.js
@@ -32,7 +32,7 @@ app.get('/', async (req, res) => {
                 .filter(header => row[header].trim() !== "") // 過濾掉空值欄位
                 .map(header => `"${header}":"${row[header]}"`)
                 .join(';');
-        }).join('\n');
+        }).join('\n\n----\n\n');
 
         // 回傳結果
         res.send(result);
