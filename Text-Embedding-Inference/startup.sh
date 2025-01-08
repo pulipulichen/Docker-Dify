b99977071ad13400f803e532f9d52b6020b093d3
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-01-09 00:36:36 +0800

    西元2025年01月09日 (週四) 00時36分36秒 CST

--- Text-Embedding-Inference/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-model=BAAI/bge-reranker-large
-volume=$PWD/data # share a volume with the Docker container to avoid downloading weights every run
-
-docker stop text-embedding-inference-app
-docker rm text-embedding-inference-app
-docker run -p 38086:80 --name text-embedding-inference-app -v $volume:/data --pull always ghcr.io/huggingface/text-embeddings-inference:cpu-1.6 --model-id $model &
--- Text-Embedding-Inference/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker stop text-embedding-inference-app
